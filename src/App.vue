983fd6e0f256005d1209a09a98eb513e6e95f999
ponurakk <p0unrakg@int.pl>
2022-12-15 21:06:33 +0100

    nav

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <router-view/>
 </template>
 
+
+<style lang="less" src="@/assets/style/global.less"></style>
+<!-- 
 <style lang="less">
 @tailwind base;
 @tailwind components;
@@ -16,27 +19,28 @@ h1, h2, h3, h4, h5, h6, h1 a, h2 a, h3 a, h4 a, h5 a, h6 a{
   margin: 0px;
 }
 
-h1 {
-  font-size: 45px;
-}
+// h1 {
+//   font-size: 45px;
+// }
 
-h2 {
-  font-size: 30px;
-}
+// h2 {
+//   font-size: 30px;
+// }
 
-h3 {
-  font-size: 25px;
-}
+// h3 {
+//   font-size: 25px;
+// }
 
-h4 {
-  font-size: 20px;
-}
+// h4 {
+//   font-size: 20px;
+// }
 
-h5 {
-  font-size: 18px;
-}
+// h5 {
+//   font-size: 18px;
+// }
 
-h6 {
-  font-size: 16px;
-}
+// h6 {
+//   font-size: 16px;
+// }
 </style>
+ -->
--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,60 +1,57 @@
 <template>
-  <header class="header">
-    <div class="content">
-      <img src="http://www.zstrzeszow.pl/img/svg_icons/zst_logo.svg" alt="">
-      <h1>ZESPÓŁ SZKÓŁ TECHNICZNYCH</h1>
-      <h3>im. Eugeniusza Kwiatkowskiego w Rzeszowie</h3>
+    <div class="home">
+        <header class="header">
+            <div class="content">
+                <img src="http://www.zstrzeszow.pl/img/svg_icons/zst_logo.svg" alt="">
+                <h1>ZESPÓŁ SZKÓŁ TECHNICZNYCH</h1>
+                <h3>im. Eugeniusza Kwiatkowskiego w Rzeszowie</h3>
+            </div>
+        </header>
+        <nav class="navbar" :class="{ 'is-hidden': !showHeader }">
+            <ul>
+                <li>O nas</li>
+                <li>Plan lekcji</li>
+                <li>E-dziennik</li>
+                <li>Rekrutacja</li>
+                <li>Lokalizacja</li>
+            </ul>
+        </nav>
     </div>
-  </header>
-  <nav class="navbar">
-    <ul>
-      <li>O nas</li>
-      <li>Plan lekcji</li>
-      <li>E-dziennik</li>
-      <li>Rekrutacja</li>
-      <li>Lokalizacja</li>
-    </ul>
-  </nav>
 </template>
-<style scoped lang="less">
-.navbar {
-  padding: 0;
-  background-color: #D9D9D9;
-  width: 80%;
-  margin: auto;
-  position: relative;
-  top: -40px;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 
-  ul {
-    li {
-      display: inline-block;
-      padding-right: 30px;
-      font-size: 30px;
-      cursor: pointer;
+
+<script>
+export default {
+    data: () => ({
+        showHeader: true,
+        scrollOffset: 750,
+    }),
+    async created() {
+        window.addEventListener('scroll', this.onScroll)
+    },
+    beforeUnmount() {
+        window.removeEventListener('scroll', this.onScroll)
+    },
+    methods: {
+        // Toggle if navigation is shown or hidden
+        onScroll() {
+            // let nav = document.querySelector('.navbar').getBoundingClientRect();
+            // console.log(nav);
+            // let contact = document.getElementById('contact').getBoundingClientRect();
+
+            // // console.log(`${projects.top}: ${contact.top}`);
+            // if (0 > projects.top-300 && contact.top > 800) {
+            //     this.$router.push("#projects");
+            // } else if (contact.top < 800) {
+            //     this.$router.push("#contact");
+            // } else {
+            //     this.$router.push("");
+            // }
+            this.showHeader = window.pageYOffset < this.scrollOffset
+        },
     }
-  }
 }
-.header {
-  background-image: url("https://cdn.discordapp.com/attachments/1051812367723409468/1053027734550491196/image.png");
-  background-size: cover;
-  min-height: 870px;
-  .content {
-    padding: 10%;
-    h1 {
-      text-align: center;
-      color: white;
-    }
-    h3 {
-      text-align: center;
-      color: white;
-    }
-    img {
-      margin: auto;
-    }
-  }
-}
+</script>
+
+<style scoped lang="less" src="@/assets/style/views/home.less">
 </style>
