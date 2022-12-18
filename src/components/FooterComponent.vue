bc1a9524930f9cb0416f0ff47e098fc1d5441d44
ponurakk <p0unrakg@int.pl>
2022-12-18 12:19:29 +0100

    it's not just nav

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="sponsors">
-    <img src="http://www.zstrzeszow.pl/img/partner_logos/xzetorzeszow.png.pagespeed.ic.mgpsVZUxQq.webp" alt="">
-    <img src="http://www.zstrzeszow.pl/img/partner_logos/xfibrain.png.pagespeed.ic.twamiqpulL.webp" alt="">
-    <img src="http://www.zstrzeszow.pl/img/partner_logos/xasseco_white.png.pagespeed.ic.236UJLdCwU.webp" alt="">
-    <img src="http://www.zstrzeszow.pl/img/partner_logos/xroxart.png.pagespeed.ic.I8Q2Z97vtd.webp" alt="">
-    <img src="http://www.zstrzeszow.pl/img/partner_logos/xpratt_w_white.png.pagespeed.ic.GTJG363hqM.webp" alt="">
-  </div>
-  <footer>
-    <h3>&copy; 2022 Concept, Design & Code: <a href="">ycode</a></h3>
-  </footer>
+    <div class="footer">
+        <div class="sponsors">
+            <img src="http://www.zstrzeszow.pl/img/partner_logos/xzetorzeszow.png.pagespeed.ic.mgpsVZUxQq.webp" alt="">
+            <img src="http://www.zstrzeszow.pl/img/partner_logos/xfibrain.png.pagespeed.ic.twamiqpulL.webp" alt="">
+            <img src="http://www.zstrzeszow.pl/img/partner_logos/xasseco_white.png.pagespeed.ic.236UJLdCwU.webp" alt="">
+            <img src="http://www.zstrzeszow.pl/img/partner_logos/xroxart.png.pagespeed.ic.I8Q2Z97vtd.webp" alt="">
+            <img src="http://www.zstrzeszow.pl/img/partner_logos/xpratt_w_white.png.pagespeed.ic.GTJG363hqM.webp" alt="">
+        </div>
+        <footer>
+            <h3>&copy; 2022 Concept, Design & Code: <a href="https://ycode.ovh">ycode</a></h3>
+        </footer>
+    </div>
 </template>
 <script>
 export default {
-  name: 'FooterComponent'
+    name: 'FooterComponent'
 }
 </script>
 <style scoped lang="less" src="@/assets/style/components/footer.less" />
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,38 +1,69 @@
 <template>
-  <header class="header">
-    <div class="icons">
-      <img src="http://www.zstrzeszow.pl/img/svg_icons/flag.svg" alt="">
-      <img src="http://www.zstrzeszow.pl/img/svg_icons/eagle.svg" alt="">
-    </div>
-    <div class="content">
-      <img src="http://www.zstrzeszow.pl/img/svg_icons/zst_logo.svg" alt="">
-      <h1>ZESPÓŁ SZKÓŁ TECHNICZNYCH</h1>
-      <h3>im. Eugeniusza Kwiatkowskiego w Rzeszowie</h3>
-    </div>
-  </header>
-  <nav class="navbar" id="navbar" :class="{ 'is-hidden': !showHeader }">
-    <ul>
-      <li v-for="(element, index) in elements" :key="index">{{ element }}</li>
-    </ul>
-  </nav>
+    <header class="header">
+        <div class="icons">
+            <img src="http://www.zstrzeszow.pl/img/svg_icons/flag.svg" alt="">
+            <img src="http://www.zstrzeszow.pl/img/svg_icons/eagle.svg" alt="">
+        </div>
+        <div class="content">
+            <img src="http://www.zstrzeszow.pl/img/svg_icons/zst_logo.svg" alt="">
+            <h1>ZESPÓŁ SZKÓŁ TECHNICZNYCH</h1>
+            <h3>im. Eugeniusza Kwiatkowskiego w Rzeszowie</h3>
+        </div>
+    </header>
+    <nav class="navbar" id="navbar" :class="{ 'is-hidden': !showHeader }">
+        <template v-for="(element, i) in elements" :key="i">
+            <a :href="element.path" v-if="element.path.startsWith('http')" class="hua">{{ element.name }}</a>
+            <router-link :to="element.path" v-else class="hua">{{ element.name }}</router-link>
+        </template>
+    </nav>
 </template>
 
 <script>
 export default {
-  name: 'NavbarComponent',
-  data: () => ({
-    showHeader: true,
-    scrollOffset: 800,
-    elements: ['O nas', 'Plan lekcji', 'E-dziennik', 'Rekrutacja', 'Lokalizacja']
-  }),
-  created() {
-    window.addEventListener('scroll', this.onScroll)
-  },
-  methods: {
-    onScroll() {
-      this.showHeader = window.pageYOffset <= this.scrollOffset;
+    name: 'NavbarComponent',
+    data: () => ({
+        showHeader: true,
+        fixed: false,
+        elements: [
+            {
+                name: "O nas",
+                path: "/about"
+            },
+            {
+                name: "Plan lekcji",
+                path: "/timetable"
+            },
+            {
+                name: "E-dziennik",
+                path: "https://adfslight.resman.pl/LoginPage.aspx"
+            },
+            {
+                name: "Rekrutacja",
+                path: "/recrutation"
+            },
+            {
+                name: "Lokalizacja",
+                path: "/localization"
+            }
+        ]
+    }),
+    computed: {
+        isExternalLink() {
+            return typeof this.to === 'string' && this.to.startsWith('http')
+        },
+    },
+    created() {
+        window.addEventListener('scroll', this.onScroll)
+    },
+    beforeUnmount() {
+        window.removeEventListener('scroll', this.onScroll)
+    },
+    methods: {
+        onScroll() {
+            let header = document.querySelector('.header').getBoundingClientRect();
+            this.showHeader = window.pageYOffset < header.height
+        }
     }
-  }
 }
 </script>
 
