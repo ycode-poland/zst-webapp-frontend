3378bb446e91742467af18e9f2b98b161eac94f3
ponurakk <p0unrakg@int.pl>
2022-12-18 13:11:45 +0100

    Changed sponsors a little

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="footer">
         <div class="sponsors">
-            <img src="http://www.zstrzeszow.pl/img/partner_logos/xzetorzeszow.png.pagespeed.ic.mgpsVZUxQq.webp" alt="">
-            <img src="http://www.zstrzeszow.pl/img/partner_logos/xfibrain.png.pagespeed.ic.twamiqpulL.webp" alt="">
-            <img src="http://www.zstrzeszow.pl/img/partner_logos/xasseco_white.png.pagespeed.ic.236UJLdCwU.webp" alt="">
-            <img src="http://www.zstrzeszow.pl/img/partner_logos/xroxart.png.pagespeed.ic.I8Q2Z97vtd.webp" alt="">
-            <img src="http://www.zstrzeszow.pl/img/partner_logos/xpratt_w_white.png.pagespeed.ic.GTJG363hqM.webp" alt="">
+            <a href="https://zetorzeszow.pl/">
+                <img src="http://www.zstrzeszow.pl/img/partner_logos/xzetorzeszow.png.pagespeed.ic.mgpsVZUxQq.webp" alt="">
+            </a>
+            <a href="https://fibrain.pl/">
+                <img src="http://www.zstrzeszow.pl/img/partner_logos/xfibrain.png.pagespeed.ic.twamiqpulL.webp" alt="">
+            </a>
+            <a href="https://pl.asseco.com/">
+                <img src="http://www.zstrzeszow.pl/img/partner_logos/xasseco_white.png.pagespeed.ic.236UJLdCwU.webp" alt="">
+            </a>
+            <a href="https://roxart.pl/">
+                <img src="http://www.zstrzeszow.pl/img/partner_logos/xroxart.png.pagespeed.ic.I8Q2Z97vtd.webp" alt="">
+            </a>
+            <a href="https://pwrze.com/">
+                <img src="http://www.zstrzeszow.pl/img/partner_logos/xpratt_w_white.png.pagespeed.ic.GTJG363hqM.webp" alt="">
+            </a>
         </div>
         <footer>
             <h3>&copy; 2022 Concept, Design & Code: <a href="https://ycode.ovh">ycode</a></h3>
