1dd59d920d2e5914b28a7359e3cf31b6e3ebb869
spaceKumpel <michalpociask11@o2.pl>
2023-01-18 20:39:46 +0100

    rekrutacja

--- src/views/application/ApplicationView.vue
+++ src/views/application/ApplicationView.vue
@@ -2,25 +2,51 @@
     <div>
         <div class="application-cont">
             <div class="application">
-                <h1>Strefa rakrutacji</h1>
+                <h1>Strefa rekrutacji</h1>
                 <h2>Harmonogram czynności w postępowaniu rekrutacyjnym na rok szkolny 2022/2023</h2>
                 <h3>Regulacje prawne czynności w postępowaniu rekrutacyjnym:</h3>
                 <ul>
-                    <li>20 ust 1 pkt 2 Ustawy o systemie oświaty z dnia 7 września 1997r.;</li>
+                    <li>20 ust 1 pkt 2 Ustawy o systemie oświaty z dnia 7 września 1997r.</li>
                     <li>154 ust.1 pkt 2 ustawy z dnia 14 grudnia 2016 r. Prawo oświatowe i przepisami wprowadzającymi – Prawo oświatowe/Dz.U.z 2017 r.poz.60 (z późn. zmianami)</li>
                     <li><span>ROZPORZĄDZENIE MINISTRA EDUKACJI NARODOWEJ</span> z dnia 16 marca 2017 r. w sprawie przeprowadzania postępowania rekrutacyjnego oraz postępowania uzupełniającego do publicznych przedszkoli, szkół i placówek.</li>
-                    <li>Zarządzenia Nr 1/2022 Podkarpackiego Kuratora Oświaty z dnia 28 stycznia 2022r. w sprawie w sprawie określenia terminów przeprowadzania postępowania rekrutacyjnego oraz postępowania uzupełniającego, w tym terminy składania dokumentów, do klas I publicznych szkół ponadpodstawowych, na semestr pierwszy klas I publicznych szkół policealnych i publicznych branżowych szkół II stopnia oraz do publicznych szkół podstawowych dla dorosłych i liceów ogólnokształcących dla dorosłych, na rok szkolny 2022/2023 w województwie podkarpackim</li>
+                    <li>Zarządzenia Nr 1/2022 Podkarpackiego Kuratora Oświaty z dnia 28 stycznia 2022r. w sprawie w sprawie określenia terminów przeprowadzania postępowania rekrutacyjnego oraz postępowania uzupełniającego, w tym terminy składania dokumentów, do klas I publicznych szkół ponadpodstawowych, na semestr pierwszy klas I publicznych szkół policealnych i publicznych branżowych szkół II stopnia oraz do publicznych szkół podstawowych dla dorosłych i liceów ogólnokształcących dla dorosłych, na rok szkolny 2022/2023 w województwie podkarpackim.</li>
                 </ul>
-                <a href="http://www.zstrzeszow.pl/documents/rekrutacja/regulamin_rekr_2022_2023.pdf">Regulamin Rekrutacji na rok szkolny 2021-2022</a>
-                <a href="http://www.zstrzeszow.pl/documents/rekrutacja/terminy_rekr_2022_2023.pdf">Harmonogram rekrutacji na rok szkolny 2021-2022</a>
-                <h3>Terminy Rekrutacji na rok szkolny 2022/2023:</h3>
-                <p>Składanie podań przez kandydatów i ich elektroniczna rejestracja następuje poprzez zalogowanie się na stronie internetowej : <a href="https://naborpg.resman.pl/kandydat/app/" class="rekru-link">strona internetowa</a></p>
-                <p>	Złożenie wniosku (w tym zmiana wniosku)<span> podpisanego przez co najmniej jednego rodzica/opiekuna prawnego</span> o przyjęcie do szkoły ponadpodstawowej.</p>
+                <a href="http://www.zstrzeszow.pl/documents/rekrutacja/regulamin_rekr_2022_2023.pdf">Regulamin Rekrutacji na rok szkolny 2022-2023</a>
+                <a href="http://www.zstrzeszow.pl/documents/rekrutacja/terminy_rekr_2022_2023.pdf">Harmonogram rekrutacji na rok szkolny 2022-2023</a>
+                <p>Składanie podań przez kandydatów i ich elektroniczna rejestracja następuje poprzez zalogowanie się na stronie internetowej : <a href="https://www.vulcan.edu.pl/programy/nabor-vulcan-52" class="rekru-link">strona internetowa</a></p>
             </div>
             <div class="info-application">
-                <h1>Streafa informacji</h1>
-                <h2></h2>
-                <h3></h3>
+                <h1>Strefa informacji</h1>
+                <h3 class="h3-color">O przyjęciu kandydatów do poszczególnych typów szkół (Technikum Nr 9, Branżowa Szkoła I Stopnia Nr 10, XIV Liceum Ogólnokształcące) decydują kryteria zawarte w rozporządzeniu MEN oraz wytycznych Podkarpackiego Kuratora Oświaty. Uwzględniają one:</h3>
+                <ul>
+                    <li>liczbę punktów możliwych do uzyskania za wyniki egzaminu przeprowadzonego w ostatnim roku nauki w szkole podstawowej, zawarte w zaświadczeniu o szczegółowych wynikach egzaminu</li>
+                    <li>oceny z języka polskiego i trzech wybranych zajęć edukacyjnych</li>
+                    <li>inne osiągnięcia ucznia wymienione w świadectwie ukończenia szkoły podstawowej</li>
+                </ul>
+                <h3><span>Wykaz przedmiotów, z których będą przeliczane punkty w procesie rekrutacji absolwentów szkoły podstawowej w 2022r.</span></h3>
+                <p><span>Technikum Nr 9</span> – w zawodzie: technik informatyk, technik elektronik, technik programista, technik mechanik, technik reklamy : </p>
+                <ul>
+                    <li>język polski,</li>
+                    <li>język obcy nowożytny (najwyższa ocena),</li>
+                    <li>matematyka,</li>
+                    <li>informatyka,</li>
+                </ul>
+                <p><span>Liceum Ogólnokształcace Nr XIV : </span></p>
+                <ul>
+                    <li>język polski,</li>
+                    <li>język obcy nowożytny (najwyższa ocena),</li>
+                    <li>matematyka,</li>
+                    <li>informatyka,</li>
+                </ul>
+                <p><span>Szkoła Branżowa I stopnia Nr 10</span> – w zawodzie: elektronik, elektromechanik, mechanik – monter maszyn I urządzeń, operator maszyn i urządzeń odlewniczych : </p>
+                <ul>
+                    <li>język polski,</li>
+                    <li>język obcy nowożytny (najwyższa ocena),</li>
+                    <li>matematyka,</li>
+                    <li>informatyka,</li>
+                </ul>
+                <p class="sekre">Szczegółowych informacji w sprawie rekrutacji udziela sekretariat szkoły:
+                    <span class="sekre-tell">tel. 17 748-31-40 lub 17 748-31-43.</span></p>
             </div>
         </div>
     </div>
