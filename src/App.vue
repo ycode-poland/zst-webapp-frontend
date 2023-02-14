fae1929aae5420897290630f0089e8e12f1b29cf
Wiktor Wilk <mcserw4@wp.pl>
2023-02-14 21:14:50 +0100

    setup loader

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <PageLoader/>
   <router-view/>
 </template>
+<script setup>
+import PageLoader from "@/components/PageLoader.vue";
+</script>
 <style lang="less" src="@/assets/style/global.less"/>
---
+++ src/components/PageLoader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page-loader" v-if="isloaded">
+    <img src="@/assets/images/zst_logo.svg" alt="">
+  </div>
+</template>
+<script>
+export default {
+  name: 'PageLoader',
+  data: () => ({
+    isloaded: true
+  }),
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState === 'complete') {
+        setInterval(() => {
+          this.isloaded = false;
+        }, 500)
+      }
+    }
+  }
+}
+</script>
+<style scoped lang="less" src="@/assets/style/global.less" />
