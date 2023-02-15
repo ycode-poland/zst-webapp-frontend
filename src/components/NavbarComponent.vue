560eaf0fd5176cd403a86e1d614b9ac51d71c3b8
spaceKumpel <michalpociask11@o2.pl>
2023-02-15 16:35:21 +0100

    asd

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,24 +1,24 @@
 <template>
-    <header class="header">
-        <div class="icons">
-            <img src="@/assets/images/flag.svg" alt="">
-            <img src="@/assets/images/eagle.svg" alt="">
-        </div>
-        <div class="content">
-            <img src="@/assets/images/zst_logo.svg" alt="">
-            <h1>ZESPÓŁ SZKÓŁ TECHNICZNYCH</h1>
-            <h3>im. Eugeniusza Kwiatkowskiego w Rzeszowie</h3>
-        </div>
-    </header>
-    <nav class="navbar" id="navbar" v-if="!$route.name.startsWith('timetable')" :class="{ 'is-hidden': !showHeader }">
-        <router-link to="/" @click="scrollToTop">
-            <font-awesome-icon icon="fa-solid fa-house" />
-        </router-link>
-        <template v-for="(element, i) in elements" :key="i">
-            <a :href="element.path" v-if="element.path.startsWith('http')" class="hua" >{{ element.name }}</a>
-            <router-link :to="element.path" @click="scrollToTop" v-else class="hua">{{ element.name }}</router-link>
-        </template>
-    </nav>
+  <header class="header">
+    <div class="icons">
+      <img src="@/assets/images/flag.svg" alt="">
+      <img src="@/assets/images/eagle.svg" alt="">
+    </div>
+    <div class="content">
+      <img src="@/assets/images/zst_logo.svg" alt="">
+      <h1>ZESPÓŁ SZKÓŁ TECHNICZNYCH</h1>
+      <h3>im. Eugeniusza Kwiatkowskiego w Rzeszowie</h3>
+    </div>
+  </header>
+  <nav class="navbar" id="navbar" v-if="!$route.name.startsWith('timetable')" :class="{ 'is-hidden': !showHeader }">
+    <router-link to="/" @click="scrollToTop">
+      <font-awesome-icon icon="fa-solid fa-house" />
+    </router-link>
+    <template v-for="(element, i) in elements" :key="i">
+      <a :href="element.path" v-if="element.path.startsWith('http')" class="hua" >{{ element.name }}</a>
+      <router-link :to="element.path" @click="scrollToTop" v-else class="hua">{{ element.name }}</router-link>
+    </template>
+  </nav>
 </template>
 
 <script>
