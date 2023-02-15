91627c28114372cba5ecd564e53adbb09f01dc22
spaceKumpel <michalpociask11@o2.pl>
2023-02-15 16:28:35 +0100

    sad

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,20 +1,22 @@
 <template>
     <header class="header">
         <div class="icons">
-            <img src="http://www.zstrzeszow.pl/img/svg_icons/flag.svg" alt="">
-            <img src="http://www.zstrzeszow.pl/img/svg_icons/eagle.svg" alt="">
+            <img src="@/assets/images/flag.svg" alt="">
+            <img src="@/assets/images/eagle.svg" alt="">
         </div>
         <div class="content">
-            <img src="http://www.zstrzeszow.pl/img/svg_icons/zst_logo.svg" alt="">
+            <img src="@/assets/images/zst_logo.svg" alt="">
             <h1>ZESPÓŁ SZKÓŁ TECHNICZNYCH</h1>
             <h3>im. Eugeniusza Kwiatkowskiego w Rzeszowie</h3>
         </div>
     </header>
     <nav class="navbar" id="navbar" v-if="!$route.name.startsWith('timetable')" :class="{ 'is-hidden': !showHeader }">
-        <router-link to="/"><font-awesome-icon icon="fa-solid fa-house"/></router-link>
+        <router-link to="/" @click="scrollToTop">
+            <font-awesome-icon icon="fa-solid fa-house" />
+        </router-link>
         <template v-for="(element, i) in elements" :key="i">
             <a :href="element.path" v-if="element.path.startsWith('http')" class="hua" >{{ element.name }}</a>
-            <router-link :to="element.path" v-else class="hua">{{ element.name }}</router-link>
+            <router-link :to="element.path" @click="scrollToTop" v-else class="hua">{{ element.name }}</router-link>
         </template>
     </nav>
 </template>
